221380ca01976ba59dcff8808bd27c064a34b93a
beepscore <support@beepscore.com>
2010-03-14 01:26:49 -0800

    Add constants, ivars, and methods from Dudney Ch 13.
    Delete sendBarButton, instead send number when user dismisses keyboard.
    Add startQuitButton.

--- Classes/GuessTheNumberViewController.h
+++ Classes/GuessTheNumberViewController.h
@@ -9,26 +9,37 @@
 #import <UIKit/UIKit.h>
 #import <GameKit/GameKit.h>
 
+#define TIME_KEY @"time"
+//protocolkeys
+#define START_GAME_KEY @"startgame"
+#define END_GAME_KEY @"endgame"
+
 @interface GuessTheNumberViewController : UIViewController 
 <GKPeerPickerControllerDelegate, GKSessionDelegate, UITextFieldDelegate> {
+    
     GKSession *gameSession;
+    NSString *opponentID;
+
     UILabel *instructionRangeLabel;
     UITextField *myNumberField;
     UILabel *opponentNumberLabel;
-    UIBarButtonItem *sendBarButton;
+    UIBarButtonItem *startQuitButton;
+    BOOL isGameHost;
+    BOOL playerWins;
+
 }
 
 @property(nonatomic, retain)GKSession *gameSession;
 @property(nonatomic, retain)IBOutlet UILabel *instructionRangeLabel;
-
 @property(nonatomic, retain)IBOutlet UITextField *myNumberField;
 @property(nonatomic, retain)IBOutlet UILabel *opponentNumberLabel;
-
-@property(nonatomic, retain)IBOutlet UIBarButtonItem *sendBarButton;
+@property(nonatomic,retain)IBOutlet UIBarButtonItem *startQuitButton;
+@property(nonatomic,assign)BOOL isGameHost;
+@property(nonatomic,assign)BOOL playerWins;
 
 
 - (IBAction)startAGame:(id)sender;
-- (IBAction)sendNumber:(id)sender;
+- (void)sendNumber:(id)sender;
 
 
 @end
--- Classes/GuessTheNumberViewController.m
+++ Classes/GuessTheNumberViewController.m
@@ -12,6 +12,9 @@
 #import "GuessTheNumberViewController.h"
 #import "Debug.h"
 
+const NSInteger kMinimum = 1;
+const NSInteger kMaximum = 10;
+
 @implementation GuessTheNumberViewController
 
 @synthesize gameSession;
@@ -19,15 +22,15 @@
 @synthesize instructionRangeLabel;
 @synthesize myNumberField;
 @synthesize opponentNumberLabel;
-@synthesize sendBarButton;
+@synthesize startQuitButton;
+@synthesize isGameHost;
+@synthesize playerWins;
 
-const NSInteger kMinimum = 1;
-const NSInteger kMaximum = 10;
 
 // the program generates theAnswer, players try to guess it.
 NSInteger theAnswer;
 
-
+#pragma mark helper methods
 // Ref http://stackoverflow.com/questions/1131101/whats-wrong-with-this-randomize-function
 // Note this works for arguments in either algebraic order.  i.e. it works if minimum > maximum
 - (float)randomFloatBetweenMin:(float)minimum andMax:(float)maximum {
@@ -40,32 +43,18 @@ NSInteger theAnswer;
 }
 
 
-// Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
-- (void)enableUI:(BOOL)enableUI {
-    
+- (void)enableUI:(BOOL)enableUI {    
     if (! enableUI) {
         self.instructionRangeLabel.hidden = YES;
         self.myNumberField.hidden = YES;
-        self.sendBarButton.enabled = NO;        
     } else {
-        NSString *tempInstruction = [[NSString alloc]
-                                     initWithFormat:@"Please enter a number between %d and %d",
-                                     kMinimum, kMaximum];    
-        self.instructionRangeLabel.text = tempInstruction;
-        [tempInstruction release];
-        
-        theAnswer = [self randomIntegerBetweenMin:kMinimum andMax:kMaximum];
-                
         self.instructionRangeLabel.hidden = NO;
         self.myNumberField.hidden = NO;
-        self.sendBarButton.enabled = YES; 
-        
-        DLog(@"instruction range label %@", self.instructionRangeLabel.text);
-        DLog(@"theAnswer = %d", theAnswer); 
     }
 }
 
 
+// Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
 - (void)viewDidLoad {
     [super viewDidLoad];
     [self enableUI:NO];
@@ -73,7 +62,6 @@ NSInteger theAnswer;
 #pragma mark -
 
 
-#pragma mark memory management methods
 - (void)didReceiveMemoryWarning {
     // Releases the view if it doesn't have a superview.
     [super didReceiveMemoryWarning];
@@ -94,7 +82,7 @@ NSInteger theAnswer;
         self.instructionRangeLabel = nil;
         self.myNumberField = nil;
         self.opponentNumberLabel = nil;
-        self.sendBarButton = nil;
+        self.startQuitButton = nil;
     }
     [super setView:newView];
 }
@@ -105,9 +93,73 @@ NSInteger theAnswer;
     [instructionRangeLabel release], instructionRangeLabel = nil;
     [myNumberField release], myNumberField = nil;
     [opponentNumberLabel release], opponentNumberLabel = nil;
-    [sendBarButton release], sendBarButton = nil;
+    [startQuitButton release], startQuitButton = nil;
     [super dealloc];
 }
+
+
+#pragma mark game methods
+// Ref Dudney sec 13.6
+-(void) updateTapCountLabels {
+}
+
+-(void) initGame {
+    NSString *tempInstruction = [[NSString alloc]
+                                 initWithFormat:@"Please enter a number between %d and %d",
+                                 kMinimum, kMaximum];    
+    self.instructionRangeLabel.text = tempInstruction;
+    [tempInstruction release];        
+    
+    theAnswer = [self randomIntegerBetweenMin:kMinimum andMax:kMaximum];
+    DLog(@"theAnswer = %d", theAnswer); 
+}
+
+
+-(void) hostGame {
+	[self initGame];
+	NSMutableData *message = [[NSMutableData alloc] init];
+	NSKeyedArchiver *archiver = [[NSKeyedArchiver alloc]
+                                 initForWritingWithMutableData:message];
+	[archiver encodeBool:YES forKey:START_GAME_KEY];
+	[archiver finishEncoding];
+	NSError *sendErr = nil;
+	[self.gameSession sendDataToAllPeers: message
+                            withDataMode:GKSendDataReliable error:&sendErr];
+	if (sendErr)
+		NSLog (@"send greeting failed: %@", sendErr);
+	// change state of startQuitButton
+	self.startQuitButton.title = @"Quit";
+	[message release];
+	[archiver release];
+	[self updateTapCountLabels];
+}
+
+-(void) joinGame {
+	[self initGame];
+	self.startQuitButton.title = @"Quit";
+    //	[self updateTapCountLabels];
+}
+
+
+-(void) showEndGameAlert {	
+    self.playerWins = YES;
+    UIAlertView *endGameAlert = [[UIAlertView alloc]
+                                 initWithTitle: self.playerWins ? @"Victory!" : @"Defeat!"
+                                 message: self.playerWins ? @"You guessed the number!": @"You lose."
+                                 delegate:nil
+                                 cancelButtonTitle:@"OK"
+                                 otherButtonTitles:nil];
+    [endGameAlert show];
+    [endGameAlert release];
+}
+
+-(void) endGame {
+	opponentID = nil;
+	self.startQuitButton.title = @"Find";
+	[self.gameSession disconnectFromAllPeers];
+	[self showEndGameAlert];
+}
+#pragma mark -
 
 
 #pragma mark UI event handlers
@@ -129,24 +181,29 @@ NSInteger theAnswer;
 
 // called when textField resigns first responder
 - (void)textFieldDidEndEditing:(UITextField *)aTextField {
-    // do nothing.  Make user press send button
+    [self sendNumber:self];
 }
 
 
 #pragma mark IBActions
+// when Start button is tapped, show peerPicker.  Ref Dudney sec 13.5
 - (IBAction)startAGame:(id)sender {
-    GKPeerPickerController *picker = [[GKPeerPickerController alloc] init];
-    picker.delegate = self;
-    [picker show];
-    [picker release], picker = nil;
+	if (! opponentID) {
+		isGameHost = YES;
+        
+		GKPeerPickerController *peerPickerController = [[GKPeerPickerController alloc] init];
+		peerPickerController.delegate = self;
+		peerPickerController.connectionTypesMask = GKPeerPickerConnectionTypeNearby;
+		[peerPickerController show];
+        [peerPickerController release], peerPickerController = nil;
+    }
 }
 
 
-- (IBAction)sendNumber:(id)sender {
+- (void)sendNumber:(id)sender {
     [self.myNumberField resignFirstResponder];
     
     NSInteger number = [self.myNumberField.text integerValue];
-    DLog(@"My number = %d", number);
     NSMutableData *message = [[NSMutableData alloc] init];
     NSKeyedArchiver *archiver = [[NSKeyedArchiver alloc] initForWritingWithMutableData:message];
     [archiver encodeInt:number forKey:@"number"];
@@ -165,7 +222,7 @@ NSInteger theAnswer;
     [self.gameSession setDataReceiveHandler:self withContext:NULL];
     NSLog(@"Peer: %@", peerID);
     [picker dismiss];
-
+    
     [self enableUI:YES];
 }
 
@@ -178,8 +235,10 @@ NSInteger theAnswer;
     { 
         case GKPeerStateConnected: 
 			[session setDataReceiveHandler: self withContext: nil]; 
-            //			opponentID = peerID;
-            //			actingAsHost ? [self hostGame] : [self joinGame];
+            
+            opponentID = peerID;
+            
+            isGameHost ? [self hostGame] : [self joinGame];
             [self enableUI:YES];
 			break;
         case GKPeerStateDisconnected: 
@@ -191,7 +250,7 @@ NSInteger theAnswer;
 
 - (void)session:(GKSession *)session
 didReceiveConnectionRequestFromPeer:(NSString *)peerID {
-    //	actingAsHost = NO;
+    self.isGameHost = NO;
 }
 
 
@@ -205,21 +264,20 @@ connectionWithPeerFailed:(NSString *)peerID
 didFailWithError:(NSError *)error {
 	NSLog (@"session:didFailWithError:");		
 }
-#pragma mark -
 
-
-
+// Ref Dudney sec 13.8
+// TODO: add endGame and joinGame messages !!!!!!!!!!!!!!!!!
 #pragma mark receive data handler
 - (void) receiveData:(NSData *)data 
-            fromPeer:(NSString *)peer 
+            fromPeer:(NSString *)peerID 
            inSession: (GKSession *)session 
              context:(void *)context {
     
     NSKeyedUnarchiver *unarchiver = [[NSKeyedUnarchiver alloc] initForReadingWithData:data];
-    NSInteger theirNumber = [unarchiver decodeIntForKey:@"number"];
-    NSString *numberString = [[NSString alloc] initWithFormat:@"%d", theirNumber];
-    self.opponentNumberLabel.text = numberString;
-    [numberString release], numberString = nil;
+    NSInteger opponentNumber = [unarchiver decodeIntForKey:@"number"];
+    NSString *opponentNumberString = [[NSString alloc] initWithFormat:@"%d", opponentNumber];
+    self.opponentNumberLabel.text = opponentNumberString;
+    [opponentNumberString release], opponentNumberString = nil;
     [unarchiver release], unarchiver = nil;
 }
 
